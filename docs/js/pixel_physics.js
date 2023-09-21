a6c9bb1e85e827b4aa88d864122e4aaf32b2f9b2
Aronnax9000 <aronnax9000@gmail.com>
2023-09-21 15:34:45 -0400

    p

--- docs/js/pixel_physics.js
+++ docs/js/pixel_physics.js
@@ -1,6 +1,6 @@
 
 
-var pixelField = [
+var pixelFieldLines = [
 "0+- +0- +-0 -+0 -0+ 0-+",
 "+-0 -+0 -0+ 0-+ 0+- +0-",
 "-0+ 0-+ 0+- +0- +-0 -+0",
@@ -21,23 +21,36 @@ var pixelField = [
 "-0+ 0-+ 0+- +0- +-0 -+0"
 ];
 
-
+var linesA = [];
 
 for (i = 0; i < 18; i++) {
-    var currentPixelField = pixelField[i];
-    var splitPixelField = currentPixelField.split(" ");
+    var lineA = [];
+    linesA.push(lineA);
+    
+    var triples = pixelFieldLines[i].split(" "); // Array of six three character strings
+    
     for (j = 0; j < 6; j++) {
-        var pixelFieldRow = splitPixelField[j];
+        var triple = triples[j]; // string of three characters
+        var tripleA = [];
+        lineA.push(tripleA);
         for (k = 0; k < 3; k++) {
-            var pixelArrayElement = pixelFieldRow.substring(k,k+1);
-            alert(pixelArrayElement);
+            tripleA.push(triple.substring(k,k+1));
         }
     }
 }
 
 
-//alert(pixelField.join());
-//$("#pixelPhysics").val(pixelField.join());
+var linesA2 = [];
+for (i=0;i<18;i++){
+    var triplesA = [];
+    for (j = 0; j < 6; j++) {
+        triplesA.push(linesA[i][j].join());
+    }
+    var line = triplesA.join(" ");
+    linesA2.push(line)
+}
+var answer = linesA2.join('\n');
+$("#textarea2").val(answer);
 
 $(document).ready(function(){
     $("#textarea1").html(pixelField.join("\n"));
