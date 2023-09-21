0fe19f07473398ac1adff607688a112f1ac0b100
Aronnax9000 <aronnax9000@gmail.com>
2023-09-21 16:51:25 -0400

    a

--- docs/js/pixel_physics.js
+++ docs/js/pixel_physics.js
@@ -53,11 +53,47 @@ function parsetext(textareaid) {
     return linesA;
 }
 
+function transformTo6633(linesA) {
+    var matrix6633 = [];
+    for(row6 = 0; row6 < 6; row6++) {
+      var row6Matrix = [];
+      matrix6633.push(row6Matrix);
+      for(col6 = 0; col6 < 6; col6++) {
+        var col6Matrix = [];
+        row6Matrix.push(col6Matrix);
+        for(row3 = 0; row3 < 3; row3++) {
+          var row3Matrix = [];
+          col6Matrix.push(row3Matrix);
+          for(col3 = 0; col3 < 3; col3++) {
+            var line = row6 * 6 + row3 * 3;
+            var triple = col6;
+            var letter = col3;
+            row3Matrix.push(linesA[line][triple][letter]);
+          }
+        }
+    }
+    return matrix6633;
+}
+
+function matrix6633ToText(matrix6633) {
+    var text = "";
+    for(line = 0; line < 18; line++) {
+        for(letter = 0; letter < 18; letter++) {
+            row6 = line % 6;
+            col6 = letter % 6;
+            row3 = line % 3;
+            col3 = letter % 3;
+            text = text.concat(matrix6633[row6][col6][row3][col3]);
+            if(col3 == 0) text = text.concat(" ");
+            if(col6 == 5 && col3 == 2) text = text.concat("\n");
+        }
+    }
+    return text;
+}
+
 function doSomething() {
     var linesA = parsetext("#textarea1");
     
-    
-
     var linesA2 = [];
     for (i=0;i<18;i++){
         var triplesA = [];
@@ -70,4 +106,7 @@ function doSomething() {
     var answer = linesA2.join('\n');
 
     $("#textarea2").html(answer);
+
+    var matrix6633 = transformTo6633(linesA);
+    $("#textarea3").html(matrix6633ToText(matrix6633));
 }
