215df346e22458debfdcee8ae7f177bfd3e89ba3
Aronnax9000 <aronnax9000@gmail.com>
2023-09-21 03:08:56 -0400

    index

--- docs/js/pixel_physics.js
+++ docs/js/pixel_physics.js
@@ -26,7 +26,7 @@ for (i = 0; i < 18; i++) {
     var currentPixelField = pixelField[i];
     alert(currentPixelField);
     var splitPixelField = currentPixelField.split(" ");
-    for (j = 0; j < 18; j++) {
+    for (j = 0; j < 6; j++) {
         var pixelFieldRow = splitPixelField[j];
         alert(pixelFieldRow);
     }
