911edde2ec804e431f3b4cbbf7a81673b94a1f77
Aronnax9000 <aronnax9000@gmail.com>
2023-09-21 03:59:38 -0400

    Complete iteration through Pixel Field.

--- docs/js/pixel_physics.js
+++ docs/js/pixel_physics.js
@@ -29,6 +29,10 @@ for (i = 0; i < 18; i++) {
     for (j = 0; j < 6; j++) {
         var pixelFieldRow = splitPixelField[j];
         alert(pixelFieldRow);
+        for (k = 0; k < 3; k++) {
+            var pixelArrayElement = pixelFieldRow.substring(k,k+1);
+            alert(pixelArrayElement);
+        }
     }
 }
 
