c877fe78abdc82470578bf096dc4e33ef3c11424
Aronnax9000 <aronnax9000@gmail.com>
2023-09-21 13:32:10 -0400

    parss

--- docs/js/pixel_physics.js
+++ docs/js/pixel_physics.js
@@ -21,15 +21,13 @@ var pixelField = [
 "-0+ 0-+ 0+- +0- +-0 -+0"
 ];
 
-/*
+
 
 for (i = 0; i < 18; i++) {
     var currentPixelField = pixelField[i];
-    alert(currentPixelField);
     var splitPixelField = currentPixelField.split(" ");
     for (j = 0; j < 6; j++) {
         var pixelFieldRow = splitPixelField[j];
-        alert(pixelFieldRow);
         for (k = 0; k < 3; k++) {
             var pixelArrayElement = pixelFieldRow.substring(k,k+1);
             alert(pixelArrayElement);
@@ -37,7 +35,7 @@ for (i = 0; i < 18; i++) {
     }
 }
 
-*/
+
 //alert(pixelField.join());
 //$("#pixelPhysics").val(pixelField.join());
 
