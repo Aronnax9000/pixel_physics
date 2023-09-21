34ebfbc3e207bea258b45a7d18f98e09beac311d
Aronnax9000 <aronnax9000@gmail.com>
2023-09-21 17:35:01 -0400

    d

--- docs/js/pixel_physics.js
+++ docs/js/pixel_physics.js
@@ -88,7 +88,7 @@ function matrix6633ToText(matrix6633) {
             col3 = letter % 3;
             console.log(row6 + ","  + row3 + "," + col6 + "," + col3);
             text = text.concat(matrix6633[row6][col6][row3][col3]);
-            if(col6 != 5 && col3 == 2) text = text.concat(" ");
+            if(col6 != 5 && col3 == 2) text = text.concat("   ");
             
         }
         if(line % 3 == 2) text = text.concat("\n");
