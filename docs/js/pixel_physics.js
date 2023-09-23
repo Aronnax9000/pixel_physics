14b17de53842e00a4c3fa4824c9d51b242ef2342
Aronnax9000 <aronnax9000@gmail.com>
2023-09-23 10:58:07 -0400

    a

--- docs/js/pixel_physics.js
+++ docs/js/pixel_physics.js
@@ -167,9 +167,9 @@ function renderPixelField() {
         evt.preventDefault();
         var target = $(evt.target).closest(".pixelField6x6Cell");
         if($(target.hasClass('selected'))) {
-            target.removeClass('selected');
+            target.find('.overlay').removeClass('selected');
         } else {
-            target.addClass('selected');
+            target.find('.overlay').addClass('selected');
         }
         var row = $(target).data('row');
         var column = $(target).data('column');
