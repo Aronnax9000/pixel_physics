a0fe187876989ee918579e1c732e52d9bd533fe1
Aronnax9000 <aronnax9000@gmail.com>
2023-09-25 12:35:16 -0400

    d

--- docs/js/pixel_physics.js
+++ docs/js/pixel_physics.js
@@ -26,6 +26,7 @@ var matrix6633;
 $(document).ready(function(){
     $("#menu").menu({position: { at: "right top" , my: "left top"}});
     $("#textDialog").dialog({autoOpen: false,
+        minWidth: 500,
         position: { my: "left top", at: "left top" }
     });
 
